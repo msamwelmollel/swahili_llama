cbc47e0b047a3bcc36065527ad91f1a01996a262
Michael Mollel <47386502+msamwelmollel@users.noreply.github.com>
2024-01-27 01:26:07 +0300

    Update merge_tokenizer.py

--- scripts/train/sentencepiece/merge_tokenizer.py
+++ scripts/train/sentencepiece/merge_tokenizer.py
@@ -22,20 +22,20 @@ parser.add_argument("--swahili_sp_model_file", default="./swahili_sp.model", typ
 args = parser.parse_args()
 
 llama_tokenizer_dir = args.llama_tokenizer_dir
-tamil_sp_model_file = args.tamil_sp_model_file
+swahili_sp_model_file = args.swahili_sp_model_file
 
 # load
 llama_tokenizer = LlamaTokenizer.from_pretrained(llama_tokenizer_dir)
-tamil_sp_model = spm.SentencePieceProcessor()
-tamil_sp_model.Load(tamil_sp_model_file)
+swahili_sp_model = spm.SentencePieceProcessor()
+swahili_sp_model.Load(swahili_sp_model_file)
 
 llama_spm = sp_pb2_model.ModelProto()
 llama_spm.ParseFromString(llama_tokenizer.sp_model.serialized_model_proto())
-tamil_spm = sp_pb2_model.ModelProto()
-tamil_spm.ParseFromString(tamil_sp_model.serialized_model_proto())
+swahili_spm = sp_pb2_model.ModelProto()
+swahili_spm.ParseFromString(swahili_sp_model.serialized_model_proto())
 
 # print number of tokens
-print(len(llama_tokenizer), len(tamil_sp_model))
+print(len(llama_tokenizer), len(swahili_sp_model))
 print(llama_tokenizer.all_special_tokens)
 print(llama_tokenizer.all_special_ids)
 print(llama_tokenizer.special_tokens_map)
@@ -44,7 +44,7 @@ print(llama_tokenizer.special_tokens_map)
 llama_spm_tokens_set = set(p.piece for p in llama_spm.pieces)
 print(len(llama_spm_tokens_set))
 print(f"Before:{len(llama_spm_tokens_set)}")
-for p in tamil_spm.pieces:
+for p in swahili_spm.pieces:
     piece = p.piece
     if piece not in llama_spm_tokens_set:
         new_p = sp_pb2_model.ModelProto().SentencePiece()
@@ -57,17 +57,17 @@ print(f"New model pieces: {len(llama_spm.pieces)}")
 output_sp_dir = "merged_tokenizer_sp"
 output_hf_dir = "merged_tokenizer_hf"  # the path to save Tamil-LLaMA tokenizer
 os.makedirs(output_sp_dir, exist_ok=True)
-with open(output_sp_dir + "/tamil_llama.model", "wb") as f:
+with open(output_sp_dir + "/swahili_llama.model", "wb") as f:
     f.write(llama_spm.SerializeToString())
-tokenizer = LlamaTokenizer(vocab_file=output_sp_dir + "/tamil_llama.model")
+tokenizer = LlamaTokenizer(vocab_file=output_sp_dir + "/swahili_llama.model")
 
 tokenizer.save_pretrained(output_hf_dir)
-print(f"Tamil-LLaMA tokenizer has been saved to {output_hf_dir}")
+print(f"Swahili-LLaMA tokenizer has been saved to {output_hf_dir}")
 
 
 # Test
 llama_tokenizer = LlamaTokenizer.from_pretrained(llama_tokenizer_dir)
-tamil_llama_tokenizer = LlamaTokenizer.from_pretrained(output_hf_dir)
+swahili_llama_tokenizer = LlamaTokenizer.from_pretrained(output_hf_dir)
 print(tokenizer.all_special_tokens)
 print(tokenizer.all_special_ids)
 print(tokenizer.special_tokens_map)
@@ -75,8 +75,8 @@ text = """கோப்பையை உறுதி செய்யுமா �
 Can India secure the World Cup trophy?"""
 print("Test text:\n", text)
 llama_tokenized = llama_tokenizer.tokenize(text)
-tamil_llama_tokenized = tamil_llama_tokenizer.tokenize(text)
+swahili_llama_tokenized = swahili_llama_tokenizer.tokenize(text)
 print(f"Tokenized by LLaMA tokenizer:{llama_tokenized}")
 print(f"LLaMA tokenizer n_tokens={len(llama_tokenized)}")
-print(f"Tokenized by Tamil-LLaMA tokenizer:{tamil_llama_tokenized}")
-print(f"Tamil LLaMA tokenizer n_tokens={len(tamil_llama_tokenized)}")
+print(f"Tokenized by Swahili-LLaMA tokenizer:{swahili_llama_tokenized}")
+print(f"Swahili LLaMA tokenizer n_tokens={len(swahili_llama_tokenized)}")
